df45fec3ec6fe94937a608fb38d138164c780bcd
isaac238 <isaac@isaac238.dev>
2023-01-14 21:18:50 +0000

    feat: Initial commit (nextjs, tailwind).

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+        <title>QuickMDN | Home</title>
+        <meta name="description" content="A website to convert ITunes playlists to Spotify playlists" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+    </>
+  )
+}
