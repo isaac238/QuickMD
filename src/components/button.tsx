37d5f76efb3bd506e16e3c2f9ca9d7e0a166fd81
isaac238 <isaac@isaac238.dev>
2023-01-15 22:37:27 +0000

    feat: Switched content prop for prop.child. #5

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,36 +1,37 @@
-import { FC } from 'react';
+import { Children, FC } from 'react';
 import Image from 'next/image'
 import Link from 'next/link'
+import { isPropertySignature } from 'typescript';
 
 interface buttonProps {
-    content: string;
     disabled?: boolean;
     backgroundColor?: string;
     borderColor?: string;
     textColor?: string;
     icon?: string;
     address?: string;
-    size?: string
+    size?: string;
+    padding? : string;
+    children: React.ReactNode;
 }
 
 export const Button: FC<buttonProps> =
 ({
-    content, disabled = false, borderColor = "#ffffff", address = "#",
-    backgroundColor = "#222222", textColor = "#ffffff", icon = "", size = "5rem"
-} : buttonProps)=> {
+    children, disabled = false, borderColor = "#ffffff", address = "#", padding = ".1rem",
+    backgroundColor = "#222222", textColor = "#ffffff", icon = "", size = "5rem"} : buttonProps)=> {
 
     const buttonStyle = {
         background: backgroundColor,
         border: `${borderColor} solid .05rem`,
         color: textColor,
-        padding: ".2rem",
+        padding: padding,
         minWidth: size,
     }
 
     const isIcon = icon !== "";
     const resultContent = isIcon ?
-    <><Image src={icon} alt="" height="50" width="50" />{content}</> :
-    <>{content}</>;
+    <><Image src={icon} alt="" height="50" width="50" />{children}</> :
+    <>{children}</>;
     return (<>
         <Link href={address}>
             <span className="inline-flex justify-center items-center rounded gap-1" style={buttonStyle}>
