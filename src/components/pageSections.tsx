a84261cd93a228b2ac84c9dcae7c6dd38e340005
isaac238 <isaac@isaac238.dev>
2023-01-15 05:08:48 +0000

    feat: split pageComponents (header, footer) and link (renamed button) into separate files and moved them to a components directory. #3

---
+++ src/components/pageSections.tsx
@@ -0,0 +1,9 @@
+import {Button} from '../components/button'
+
+export const Footer: FC = ()=> {
+    return (<>
+    <footer className="flex flex-row text-white justify-center items-center text-xl font-thin p-2">
+        <h1>Copyright &copy;isaac238 (2023)</h1>
+    </footer>
+    </>)
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Link, Footer } from './components'
+import {Footer} from '../components/pageSections'
+import {Button} from '../components/button'
 
 export default function Home() {
   return (
@@ -12,13 +12,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
     </Head>
     <main className="text-white flex flex-col gap-2 justify-center items-center w-screen">
-        <h1 className="w-fit text-3xl">Quick MD</h1>
-        <section className="flex w-fit gap-2">
-            <Link content="GitHub" icon="/image/githubWhite.png" link="https://github.com/isaac238/QuickMD"/>
-            <Link content="New MD" backgroundColor="#009432"/>
+        <h1 className="w-fit text-6xl">Quick MD</h1>
+        <section className="flex w-fit gap-2 text-4xl">
+            <Button content="GitHub" icon="/image/githubWhite.png" address="https://github.com/isaac238/QuickMD"/>
+            <Button content="New MD" backgroundColor="#009432" address='createmd'/>
         </section>
     </main>
     <Footer/>
     </>
   )
 }
+
