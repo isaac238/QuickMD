8784fa995ee76dfa6e5fa3defa46d02cfdc1fb01
isaac238 <isaac@isaac238.dev>
2023-01-15 02:58:32 +0000

    fix: Corrected Quick MDN > Quick MD. #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,16 @@ export default function Home() {
   return (
     <>
     <Head>
-        <title>QuickMDN | Home</title>
+        <title>QuickMD | Home</title>
         <meta name="description" content="A website to allow the easy correction of markdown docs" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
     </Head>
     <main className="text-white flex flex-col gap-2 justify-center items-center w-screen">
-        <h1 className="w-fit text-3xl">Quick MDN</h1>
+        <h1 className="w-fit text-3xl">Quick MD</h1>
         <section className="flex w-fit gap-2">
             <Link content="GitHub" icon="/image/githubWhite.png" link="https://github.com/isaac238/QuickMD"/>
-            <Link content="New MDN" backgroundColor="#009432"/>
+            <Link content="New MD" backgroundColor="#009432"/>
         </section>
     </main>
     <Footer/>
