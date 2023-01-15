0de8d967027a505a7d5ca6501e0ca786331de612
isaac238 <isaac@isaac238.dev>
2023-01-15 02:31:25 +0000

    feat: Set global font to Roboto (google fonts). #1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto } from '@next/font/google'
 
+const roboto = Roboto({
+    weight: ['100', '300', '400', '500'],
+    subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
             <>
+            <style jsx global>{`
+                html {
+                font-family: ${roboto.style.fontFamily};
+                }
+            `}</style>
             <Component {...pageProps} />
             </>
             )
