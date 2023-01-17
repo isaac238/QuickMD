0b2f8c6eb92dca44f9ee5d6d8f9cbe81b64accfa
isaac238 <isaac@isaac238.dev>
2023-01-17 20:27:29 +0000

    refactor: Removed unused import.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,6 @@
 import { Children, FC } from 'react';
 import Image from 'next/image'
 import Link from 'next/link'
-import { isPropertySignature } from 'typescript';
 
 interface buttonProps {
     disabled?: boolean;
